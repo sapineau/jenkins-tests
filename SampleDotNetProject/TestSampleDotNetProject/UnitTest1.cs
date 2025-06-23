574f5c0c12210ebf39934f29454c151318a11b0c
Samuel Pineau <samuel.pineau.ext@coptis.com>
2025-06-23 14:56:35 +0200

    feat: initial commit to test Jekins parameters

---
+++ SampleDotNetProject/TestSampleDotNetProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace TestSampleDotNetProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.False(false);
+        }
+    }
+}
